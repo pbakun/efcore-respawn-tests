2beea2b90a0b8b2791161c90bb4e0c0af04a950f
pbakun <piotr.bakun@hotmail.com>
2023-08-01 21:15:22 +0200

    update integration.yml

--- tests/CitiesApp.IntegrationTests/Setup/CustomWebApplicationFactory.cs
+++ tests/CitiesApp.IntegrationTests/Setup/CustomWebApplicationFactory.cs
@@ -11,7 +11,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Respawn;
 using Respawn.Graph;
 using System.Data.Common;
-using Xunit.Abstractions;
 
 namespace CitiesApp.IntegrationTests.Setup
 {
